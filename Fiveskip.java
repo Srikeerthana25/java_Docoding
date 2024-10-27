663ea71a0f437bdb4d5fa408430408e8d8261893
Srikeerthana25 <109751615+Srikeerthana25@users.noreply.github.com>
2024-10-27 18:58:46 +0530

    Add files via upload

---
+++ Fiveskip.java
@@ -0,0 +1,17 @@
+// public class Fiveskip {
+    
+//     public static void main(String[] args) {
+//         for(int i = 1; i <= 100; i++){
+//             if(i % 5 == 0){
+//                 System.out.println(i + " ");
+//                 // continue;
+//             }
+            
+//         }
+//     }
+    
+// }
+
+
+
+
