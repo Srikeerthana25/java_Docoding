945bc064c6a6c0b26afe1a5b8025ae885255c0ff
Srikeerthana25 <109751615+Srikeerthana25@users.noreply.github.com>
2024-10-28 19:38:07 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,127 @@
+ /* public class Calculator {
+    public int multiply(int x,int y){
+        return x * y;
+        
+    }
+    
+   public static void main(String[] args){
+    Calculator calc = new Calculator();
+    int result = calc.multiply( 5,8);
+    System.out.println("Result=" + result);
+   }
+   
+
+    } */
+   /*public class Helloworld{
+    public static void main(String[] args){
+        System.out.println("HelloAnna");
+    }
+   } */
+
+    /*import java.util.Scanner;
+   public class Example {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name:");
+        String name = scanner.nextLine();
+        System.out.println("Sri" + name + "!!");
+    }
+   }*/
+
+
+  // import java.util.Scanner;
+  // public class Check {
+  //  public static void main(String[] args) {
+  //      Scanner scanner = new Scanner(System.in);
+  //     System.out.println("enter the number");
+  //     int age= scanner.nextInt();
+
+  //     System.out.println("are you citizen (yes/no)");
+  //     String citizenInput = scanner.next().toLowerCase();
+
+  //     boolean iscitizen = citizenInput.equals("yes");
+
+     // boolean iscitizen = scanner.nextBoolean();
+
+  //       if ( number % 2 != 0 ) {
+  //         System.out.println(number + "is odd");
+  //       }
+  //       else{
+  //         System.out.println(number + " is even");
+  //       }
+  //  if (age >= 18 && iscitizen  ) {
+  //    System.out.println("you are eligible");
+  //    }
+  //    else{
+  //      System.out.println("you are not eligible");
+  //    }
+        //if( number >= 50 && number <= 100){
+         //   System.out.println(+ number + "is the number between  50 to 100");
+        //}
+        //else{
+          //  System.out.println(+ number + "is the number between 1 to 50");
+        //}
+
+  //   }
+  // }
+     
+
+
+
+
+ /*  import java.util.Scanner;
+
+  public class Simplecalc {
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("enter the number");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("enter the number");
+        double num2 = scanner.nextDouble();
+     
+       System.out.println("+,-,*,/");   
+       char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch(operation){
+           
+           case '+':
+           result =num1 + num2;
+           break; 
+            
+
+           case '-':
+           result = num1 - num2;
+           break;
+           
+           case '*':
+           result = num1 * num2;
+           break;
+
+           case '/':
+           if (num2 != 0){
+            result = num1 / num2;
+           }
+           else{
+            System.out.println("Invalid data");
+            return;
+           }
+
+           break;
+
+           default:
+
+           System.out.println("Invalid data");
+           return;
+        }
+
+     System.out.println("the result is:" + result);
+
+      }
+    
+  } */
+
+  
